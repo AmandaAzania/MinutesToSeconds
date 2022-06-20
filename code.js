4cc69436b844c429845a2502abf4fc7c5333a351
AmandaAzania <mandygudlwa@gmail.com>
2022-06-20 14:18:17 +0200

    adding

--- code.js
+++ code.js
@@ -1,23 +1,37 @@
-function convertToSeconds(hours, minutes){
-  
-    var perHour = 60;
-    var perMin = 60;
-    var multiplyHoursAndSec;
-    var multiplyHoursAndMin;
-    var multiplyMinAndSec;
-    var totalSec = document.getElementById("calculate").innerHTML;
-    var hours = document.getElementById("hours").value;
-    var minutes = +document.getElementById("minutes").value;
-  
-    multiplyHourAndSec = hours * perHour * perMin;
-    multiplyMinAndSec = minutes * perMin;
-    totalSec = multiplyHourAndSec + multiplyMinAndSec;
-    
-    multiplyHoursAndMin = hours * perMin + minutes;
+function attachEventsListeners() {
+    let ratios = {
+       minutes: 1440,
+       seconds: 86400
+   };
    
-    
-    document.getElementById("calculate").innerHTML = multiplyHoursAndMin + " minutes, " + totalSec + " seconds";
-    
-  }
+   function convertValue(value, from) {
+       const inDays = value / ratios[from];
+       return {
+           minutes: inDays * ratios.minutes,
+           seconds: inDays * ratios.seconds
+       };
+   }
+   
   
+   const minutesDiv = document.getElementById('minutes');
+   const secondsDiv = document.getElementById('seconds');
+
   
+   document.getElementById('minutesBtn').addEventListener('click', convertTime);
+   document.getElementById('secondsBtn').addEventListener('click', convertTime);
+
+
+   function convertTime(e) {
+       let value = Number(e.target.parentNode.querySelector('input[type="text"]').value);
+       let from = e.target.parentNode.querySelector('input[type="text"]').id;
+       const convertedValues = convertValue(value, from);
+       display(convertedValues);
+   }
+   
+
+   function display(valuesToDisplay) {
+       minutesDiv.value = valuesToDisplay.minutes;
+       secondsDiv.value = valuesToDisplay.seconds;
+
+   }
+}
